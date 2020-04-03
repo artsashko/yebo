a93f3cb108376596bb5bbdfeb4749e30e1ada34b
Artyom <artyomsashko@gmail.com>
2020-04-03 23:24:19 +0300

    Исправлены недочеты в адаптиве и навигация по секциям

--- js/main.js
+++ js/main.js
@@ -250,12 +250,5 @@ $('.nav__icon').click(function(event) {
 	$('body').toggleClass('lock');
 });
 
-function ibg(){
-	$.each($('.ibg'), function(index, val) {
-		if($(this).find('img').length>0){
-			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-		}
-	});
-}
-ibg();
+
 });
--- js/script.js
+++ js/script.js
@@ -28,12 +28,7 @@ ready(() => {
 	});
 
 	//IBG
-	document.querySelectorAll(".ibg").forEach(el => {
-		if(el.querySelector('img')){
-			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
-			el.querySelector('img').style.display = 'none';
-		}
-	});
+
 });
 
 
